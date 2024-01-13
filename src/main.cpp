edca712461f406e721d5a3005539253eac6ee91d
Mark Foskett <markryanfoskett@gmail.com>
2024-01-13 19:44:16 +1100

    created snake class

---
+++ src/main.cpp
@@ -0,0 +1,17 @@
+// Snake
+#include <Arduino.h>
+
+#include "snake.h"
+
+// #include "avr8-stub.h"
+// #include "app_api.h" // only needed with flash breakpoints
+
+void setup()
+{
+  // initialize GDB stub
+  // debug_init();
+}
+
+void loop()
+{
+}
---
+++ src/snake.cpp
@@ -0,0 +1,13 @@
+#include <Arduino.h>
+
+#include "snake.h"
+
+// Constructor Function Definition
+Snake::Snake(int snakeLength)
+    : m_snakeLength{snakeLength}
+{
+}
+
+void Snake::drawSnake(){
+
+};

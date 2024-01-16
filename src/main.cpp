1cbcd2bb81630592ea92a3836ceb8f29e6ac1caa
Mark Foskett <markryanfoskett@gmail.com>
2024-01-16 19:06:45 +1100

    initialised snake position on LED matrix

--- src/main.cpp
+++ src/main.cpp
@@ -2,7 +2,8 @@
 #include <Arduino.h>
 
 #include "LedControl.h"
-// #include "snake.h"
+#include "snakeSegment.h"
+#include "snake.h"
 
 // #include "avr8-stub.h"
 // #include "app_api.h" // only needed with flash breakpoints
@@ -13,6 +14,8 @@
 //  Pin 11 is connected to LOAD(CS)
 //  # of devices = 1
 LedControl lc = LedControl(12, 10, 11, 1);
+// Create an instance of the Snake class
+Snake snake = Snake(2);
 
 void setup()
 {
@@ -26,13 +29,26 @@ void setup()
   lc.setIntensity(0, 1);
   // Clear the display
   lc.clearDisplay(0);
+  // Begin Serial Monitor
+  Serial.begin(115200);
+  // Setup the default snake (Head and 1 Tail Segment)
+  snake.tail.setStorage(snake.storage_array); // Setup the SnakeSegment storage array
+  SnakeSegment tail_1 = SnakeSegment(0, 3, NULL);
+  SnakeSegment head = SnakeSegment(1, 3, &tail_1);
+  Serial.print(snake.tail.size());
+  snake.tail.push_back(head);
+  Serial.print(snake.tail.size());
+  snake.tail.push_back(tail_1);
+  Serial.print(snake.tail.size());
 }
 
 void loop()
 {
   // Set the starting position of the snake
-  lc.setLed(0, 0, 3, true);
-  delay(50);
-  lc.setLed(0, 0, 3, false);
-  delay(50);
+  for (int i = 0; i < snake.tail.size(); i++)
+  {
+    // Print snake segment
+    lc.setLed(0, snake.tail.at(i).getX(), snake.tail.at(i).getY(), true);
+    delay(50);
+  }
 }

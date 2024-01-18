1cb0a82bbf30bbad83c667698b873f52d2ccfea0
Mark Foskett <markryanfoskett@gmail.com>
2024-01-18 23:52:42 +1100

    moving the snake

--- include/snakeSegment.h
+++ include/snakeSegment.h
@@ -7,12 +7,12 @@ class SnakeSegment
 private:
     int m_x{0};
     int m_y{0};
-    SnakeSegment *m_next{};
 
 public:
+    SnakeSegment *next{NULL};
     // Constructor Function Declaration
-    SnakeSegment();                                 // Default Constructor
-    SnakeSegment(int x, int y, SnakeSegment *next); // Explicit Constructor
+    SnakeSegment();             // Default Constructor
+    SnakeSegment(int x, int y); // Explicit Constructor
     // Member Function Declarations
 
     // Access Function Definitions
@@ -20,6 +20,8 @@ public:
     void setX(int x) { m_x = x; }    // setter for X position
     int getY() const { return m_y; } // getter for Y position
     void setY(int y) { m_y = y; }    // setter for Y position
+    // SnakeSegment *getNext() const { return m_next; }    // getter for next
+    // void setNext(SnakeSegment *next) { m_next = next; } // setter for next
 };
 
 #endif
--- src/snake.cpp
+++ src/snake.cpp
@@ -2,6 +2,7 @@
 
 #include "snake.h"
 #include "snakeSegment.h"
+#include "display.h"
 
 // Constructor Function Definition
 Snake::Snake(int snakeLength)
@@ -9,6 +10,62 @@ Snake::Snake(int snakeLength)
 {
 }
 
-void Snake::drawSnake(){
-
+void Snake::moveSnake()
+{
+    // New Snake Head
+    SnakeSegment *head = tail.front();
+    // Point the new snake head to the previous snake head
+    head->next = tail.front();
+    // Position the new snake head based on the current direction
+    int newHeadX = tail.front()->getX();
+    int newHeadY = tail.front()->getY();
+    switch (direction)
+    {
+    case UP:
+        if (newHeadX + 1 > maxRows - 1)
+        {
+            head->setX(0);
+        }
+        else
+        {
+            head->setX(newHeadX + 1);
+        }
+        break;
+    case DOWN:
+        if (newHeadX - 1 < 0)
+        {
+            head->setX(maxRows - 1);
+        }
+        else
+        {
+            head->setX(newHeadX - 1);
+        }
+        break;
+    case LEFT:
+        if (newHeadY - 1 < 0)
+        {
+            head->setY(maxCols - 1);
+        }
+        else
+        {
+            head->setY(newHeadY - 1);
+        }
+        break;
+    case RIGHT:
+        if (newHeadY + 1 > (maxCols - 1))
+        {
+            head->setY(0);
+        }
+        else
+        {
+            head->setY(newHeadY + 1);
+        }
+        break;
+    default:
+        break;
+    }
+    // Add the new snake head to the front of the snake segment tail
+    tail.front() = head;
+    // Pop the tail segment
+    tail.pop_back();
 };
